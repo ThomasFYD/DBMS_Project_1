bd2b69a50cb485dbe867587ab97c873213e51d68
Ziyi Liu <134427218+Elaiccc@users.noreply.github.com>
2024-10-20 19:53:41 -0400

    Delete school_project directory

--- school_project/school_data/forms.py
+++
@@ -1,8 +0,0 @@
-# school_data/forms.py
-from django import forms
-from .models import SchoolData
-
-class SchoolDataForm(forms.ModelForm):
-    class Meta:
-        model = SchoolData
-        fields = '__all__'  # Or specify fields you want to include
